675b2dae0d6ba82906635bb36e6b651bcd28fb20
micho19 <damiangiannico@gmail.com>
2021-01-16 22:17:37 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState, useEffect } from 'react';
+import Formulario from './Components/Formulario';
+import Cancion from './Components/Cancion';
+import Info from './Components/Info';
+import axios from 'axios';
 
 function App() {
+  // definir el state
+  const [busquedaletra, guardarBusquedaLetra] = useState({});
+  const [letra, guardarLetra] = useState('');
+  const [info, guardarInfo] = useState({});
+  const [error, guardarError] = useState(false);
+
+  useEffect(() => {
+    if (Object.keys(busquedaletra).length === 0) return;
+
+    const consultarApiLetra = async () => {
+      const { artista, cancion } = busquedaletra;
+      const url = `https://api.lyrics.ovh/v1/${artista}/${cancion}`;
+      const url2 = `https://www.theaudiodb.com/api/v1/json/1/search.php?s=${artista}`;
+
+      axios
+        .all([axios.get(url), axios.get(url2)])
+        .then(
+          axios.spread((letra, informacion) => {
+            guardarLetra(letra.data.lyrics);
+            guardarInfo(informacion.data.artists[0]);
+          })
+        )
+        .catch((error) => {
+          guardarError(true);
+        });
+      guardarError(false);
+    };
+    consultarApiLetra();
+  }, [busquedaletra]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Formulario guardarBusquedaLetra={guardarBusquedaLetra} />
+
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-md-6">
+            <Info info={info} />
+          </div>
+          <div className="col-md-6">
+            <Cancion letra={letra} />
+            {letra.length === 0 && Object.keys(info).length !== 0 ? (
+              <p className="alert alert-primary text-center">
+                No se encontró la letra Conan!
+              </p>
+            ) : null}
+          </div>
+        </div>
+      </div>
+    </Fragment>
   );
 }
 
---
+++ src/Components/Error.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Error = () => {
+  return (
+    <p className="alert alert-danger text-center p-2">
+      Todos los campos son obligatorios Conann!!!
+    </p>
+  );
+};
+
+export default Error;
